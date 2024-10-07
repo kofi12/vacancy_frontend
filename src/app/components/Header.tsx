ffcd36a0da9543a480919eae50f8b48efae38f7d
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2024-10-06 20:59:39 -0400

    init project setup with shadcn ui tooling

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = () => {
+
+  const links = [
+      {
+        name: 'Home',
+        path: '/'
+      },
+      {
+        name: '',
+        path: '/'
+      },
+      {
+        name: '',
+        path: '/'
+      },
+      {
+        name: '',
+        path: '/'
+      }
+  ]
+
+  return (
+    <div className="flex flex-row gap-8" >Header</div>
+  )
+}
+
+export default Header
